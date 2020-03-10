84a81214bef5e6a9a436bb2dd36d6774193122b3
dinhanhx <dinhanhx@gmail.com>
2020-03-10 10:53:17 +0700

    Create iris_flower_tester.py to load, run

---
+++ iris_flower_prophet_tester.py
@@ -0,0 +1,18 @@
+import pickle as pk
+from pandas import read_csv
+
+model = pk.load(open('iris_flower_prophet.pkl', 'rb'))
+
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class'];
+dataset = read_csv('iris.csv', names = names)
+
+arr = dataset.values
+X = arr[0:1, 0:4]
+Y = arr[0:1, 4]
+
+print('iris_flower_prophet said: ')
+Y_predicted = model.predict(X)
+print(Y_predicted)
+
+print('human said: ')
+print(Y)
